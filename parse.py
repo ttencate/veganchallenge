be7d392d2bd6e81dceaf8e57f44a3efe656fb7ed
Thomas ten Cate <ttencate@gmail.com>
2017-03-28 19:54:02 +0200

    Add latex stubs

--- parse.py
+++ parse.py
@@ -22,6 +22,42 @@ def cached_fetch(url, cache_key, postdata=None):
             out_file.write(data)
         return data
 
+def process_recipe(name, url):
+    recipe = cached_fetch(url, '%s.html' % name)
+    soup = BeautifulSoup(recipe, 'html.parser')
+
+    title = soup.find('h1').text.strip()
+    intro = soup.find('h1').parent
+    intro.find('h1').extract()
+    summary = intro.text.strip()
+    ingredienten, bereiding = [h2.parent for h2 in soup.find_all('h2', class_='recipetitle')]
+
+    ingredients = [li.text.strip() for li in ingredienten.find_all('li')]
+    bereiding.find('h2').extract()
+    directions = bereiding.text.strip()
+
+    with open(os.path.join('include', '%s.tex' % name), mode='wt', encoding='utf-8') as out_recipe:
+        out_recipe.write('\\begin{recipe}{%s}\n' % title)
+        out_recipe.write('\n')
+
+        out_recipe.write('\\begin{recipesummary}\n')
+        out_recipe.write('%s\n' % summary)
+        out_recipe.write('\\end{recipesummary}\n')
+        out_recipe.write('\n')
+
+        out_recipe.write('\\begin{ingredients}\n')
+        for ingredient in ingredients:
+            out_recipe.write('\\item %s\\n' % ingredient)
+        out_recipe.write('\\end{ingredients}\n')
+        out_recipe.write('\n')
+
+        out_recipe.write('\\begin{directions}\n')
+        out_recipe.write('%s\n' % directions)
+        out_recipe.write('\\end{directions}\n')
+        out_recipe.write('\n')
+
+        out_recipe.write('\\end{recipe}\n')
+
 with open(os.path.join('include', 'dagmenus.tex'), mode='wt', encoding='utf-8') as out_dagmenus:
     for date in [
             '2017-03-27',
@@ -40,7 +76,7 @@ with open(os.path.join('include', 'dagmenus.tex'), mode='wt', encoding='utf-8') 
             day = h2.text.rstrip(':').replace('  ', ' ').capitalize()
             print('# %s' % day)
 
-            out_dagmenus.write('\\day{%s}\n' % day)
+            out_dagmenus.write('\\begin{dagmenu}{%s}\n' % day)
 
             for receptitem in dagmenu.find_all(class_='receptitem'):
                 meal = receptitem.find(class_='headtitle').text.capitalize()
@@ -48,9 +84,12 @@ with open(os.path.join('include', 'dagmenus.tex'), mode='wt', encoding='utf-8') 
                 url = receptitem.find(class_='title').find('a')['href']
                 name = url.split('/')[-2]
                 print('%s: %s (%s)' % (meal, title, url))
-                recipe = cached_fetch(url, '%s.html' % name)
+
                 out_dagmenus.write('\\input{include/%s.tex}\n' % name)
+
+                process_recipe(name, url)
 
             print()
 
+            out_dagmenus.write('\\end{dagmenu}\n')
             out_dagmenus.write('\n')
