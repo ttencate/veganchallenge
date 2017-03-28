621c673739c594e847c6365c95c33c69046b6896
Thomas ten Cate <ttencate@gmail.com>
2017-03-28 17:31:31 +0200

    Parse dishes

--- parse.py
+++ parse.py
@@ -7,14 +7,15 @@ from bs4 import BeautifulSoup
 
 def cached_fetch(url, cache_key, postdata=None):
     try:
-        with open(cache_key, 'rb') as in_file:
+        with open(cache_key, mode='r', encoding='utf-8') as in_file:
             return in_file.read()
     except Exception as ex:
+        print('Fetching %s...' % url)
         if postdata is not None and postdata is not bytes:
             postdata = bytes(postdata, 'utf-8')
         with urllib.request.urlopen(url, postdata) as in_url:
-            data = in_url.read()
-        with open(cache_key, 'wb') as out_file:
+            data = in_url.read().decode('utf-8')
+        with open(cache_key, mode='w', encoding='utf-8') as out_file:
             out_file.write(data)
         return data
 
@@ -27,4 +28,14 @@ for date in [
         ]:
     week = cached_fetch('https://veganchallenge.nl/getmenu/', '%s.html' % date, 'date=%s' % date)
     soup = BeautifulSoup(week, 'html.parser')
-    print(soup.prettify())
+    for dagmenu in soup.find_all(class_='_dagmenu'):
+        h2 = dagmenu.find('h2')
+        if not h2:
+            continue
+        day = h2.text.rstrip(':').replace('  ', ' ').capitalize()
+        print('# %s' % day)
+        for receptitem in dagmenu.find_all(class_='receptitem'):
+            meal = receptitem.find(class_='headtitle').text.capitalize()
+            title = receptitem.find(class_='title').text
+            print('%s: %s' % (meal, title))
+        print()
